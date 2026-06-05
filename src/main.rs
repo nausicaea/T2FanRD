06fa09874541bc76da44e2a963c09784623871eb
Eleanor Young <developer@nausicaea.net>
2026-06-05 08:36:26 +0200

    refactor: make the empty tuple argument to a closure more explicit

--- src/main.rs
+++ src/main.rs
@@ -230,5 +230,5 @@ fn real_main() -> Result<()> {
         fan.set_manual(false)?;
     }
 
-    res.and_then(|_| std::fs::remove_file(PID_FILE).map_err(Error::PidDelete))
+    res.and_then(|()| std::fs::remove_file(PID_FILE).map_err(Error::PidDelete))
 }
